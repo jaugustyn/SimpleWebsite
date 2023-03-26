abe6e6a3d8c4fe15f5d5e32f961cb9dde9ed2cff
jaugustyn <kub4a1@gmail.com>
2023-03-26 17:37:40 +0200

    HTML code cleanup

--- functions.js
+++ functions.js
@@ -4,34 +4,34 @@ if (imageBox1 != null) {
     function galleryFunction(smallImg) {
         imageBox1.src = smallImg.src;
     }
-    
+
     // Get the modal image tag
     var modal = document.getElementById("imgModal");
-    
+
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-    
+
     var modalImage = document.getElementById("modal-image");
-    
+
     // When the user clicks the big picture, set the image and open the modal
     imageBox1.onclick = function (e) {
         var src = e.srcElement.src;
         modal.style.display = "block";
         modalImage.src = src;
     };
-    
+
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
     };
-    
+
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal || event.target == document.getElementsByClassName("modal-content")[0]) {
             modal.style.display = "none";
         }
     };
-    
+
 }
 
 // Nav items change active class on click, it adds .active class to clicked item and removes it from other one
@@ -51,7 +51,7 @@ if (imageBox1 != null) {
 
 
 
-// Change main content 
+// Change main content
 
 // var content = document.querySelector('#container');
 // var links = document.querySelectorAll('.navitem');
